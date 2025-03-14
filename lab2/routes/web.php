75eddebce3d817893db45a2b024c80505b1ecc17
tholm25 <leminhtho762@gmail.com>
2025-03-14 21:41:55 +0700

    đã sửa

---
+++ lab2/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ThongTinController;
+use App\Http\Controllers\TinController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/product', function () {
+//     return view('product');
+// });
+
+Route::get('/',[TinController::class, 'trangChu']);
+Route::get('/lienhe',[TinController::class, 'lienHe']);
+Route::get('/ct/{id}',[TinController::class, 'chiTiet']);
+Route::get('/thongtinsv',[ThongTinController::class, 'thongTin']);
+
+
+?>
+
+
+
